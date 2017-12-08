5496164858ec0dfe88ba128b5776ab642824e213
ivyzvh <ivyzvh@qq.com>
2017-12-08 15:39:31 +0800

    拆包粘包问题

---
+++ src/com/vv/time2/TimeClientHandler.java
@@ -0,0 +1,49 @@
+package com.vv.time2;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Logger;
+
+/**
+ * Netty 5
+ */
+public class TimeClientHandler extends ChannelHandlerAdapter {
+	private static final Logger logger = Logger.getLogger(TimeClientHandler.class.getName());
+
+	private final ByteBuf firstMessage;
+
+	public TimeClientHandler() {
+		System.out.println(this.getClass().getSimpleName() + ".TimeClientHandler()");
+		byte[] req = "QUERY TIME ORDER".getBytes();
+		firstMessage = Unpooled.buffer(req.length);
+		firstMessage.writeBytes(req);
+	}
+
+	//@Override
+	public void channelActive(ChannelHandlerContext ctx) throws UnsupportedEncodingException {
+		System.out.println(this.getClass().getSimpleName() + ".channelActive()");
+		ctx.writeAndFlush(firstMessage);
+	}
+
+	//@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		System.out.println(this.getClass().getSimpleName() + ".channelRead()");
+		ByteBuf buf = (ByteBuf) msg;
+		byte[] req = new byte[buf.readableBytes()];
+		buf.readBytes(req);
+		String body = new String(req, "UTF-8");
+		System.out.println("服务器应答信息 : " + body);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		System.out.println(this.getClass().getSimpleName() + ".exceptionCaught()");
+		logger.warning("Unexcepted exception from downstram : " + cause.getMessage());
+		ctx.close();
+	}
+
+}
---
+++ src/com/vv/time2/TimeServer.java
@@ -0,0 +1,52 @@
+package com.vv.time2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import org.example.hello.HelloClientHandler;
+
+/**
+ * Netty 5
+ */
+public class TimeServer {
+	
+	public void bind(int port) throws Exception {
+		System.out.println("[时间服务器]启动...");
+		EventLoopGroup bossGroup = new NioEventLoopGroup(); // 用于处理服务器端接收客户端连接
+		EventLoopGroup workerGroup = new NioEventLoopGroup(); // 进行网络通信（读写）
+		try {
+			ServerBootstrap serverBoot = new ServerBootstrap(); // 辅助工具类，用于服务器通道的一系列配置
+			serverBoot.group(bossGroup, workerGroup) // 绑定两个线程组
+			          .channel(NioServerSocketChannel.class) // 指定NIO的模式
+			          .option(ChannelOption.SO_BACKLOG, 1024)
+			          .childHandler(new ChildChannelHandler());
+			
+			// 服务器绑定端口监听
+			ChannelFuture future = serverBoot.bind(port).sync();
+			// 监听服务器关闭监听(为了防止线程停止)。
+			future.channel().closeFuture().sync();
+		} finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+
+	}
+
+	private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+		@Override
+		protected void initChannel(SocketChannel arg0) throws Exception {
+			System.out.println("[时间服务器]" + this.getClass().getSimpleName() + ".initChannel()");
+			arg0.pipeline().addLast(new TimeServerHandler());
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		new TimeServer().bind(7879);
+	}
+}
