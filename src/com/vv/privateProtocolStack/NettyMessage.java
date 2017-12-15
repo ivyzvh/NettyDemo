c65b6239d448980052869ebe1e41daa941a9a500
ivyzvh <ivyzvh@qq.com>
2017-12-15 10:13:40 +0800

    私有协议栈

---
+++ src/com/vv/privateProtocolStack/NettyMessage.java
@@ -0,0 +1,50 @@
+package com.vv.privateProtocolStack;
+/**
+ * @author lilinfeng
+ * @version 1.0
+ * @date 2014Äê3ÔÂ14ÈÕ
+ */
+public final class NettyMessage {
+
+    private Header header;
+
+    private Object body;
+
+    /**
+     * @return the header
+     */
+    public final Header getHeader() {
+        return header;
+    }
+
+    /**
+     * @param header the header to set
+     */
+    public final void setHeader(Header header) {
+        this.header = header;
+    }
+
+    /**
+     * @return the body
+     */
+    public final Object getBody() {
+        return body;
+    }
+
+    /**
+     * @param body the body to set
+     */
+    public final void setBody(Object body) {
+        this.body = body;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "NettyMessage [header=" + header + "]";
+    }
+}
